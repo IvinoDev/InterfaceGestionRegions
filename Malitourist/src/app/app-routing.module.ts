03cc38c79ab8b387e28e6df24036c0a89b2d74ea
IvinoDev <kayantao@protonmail.com>
2022-12-16 23:59:15 +0000

    test gmaps

--- Malitourist/src/app/app-routing.module.ts
+++ Malitourist/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegionComponent } from './pages/region/region.component';
 import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
   {path:"",redirectTo:"home",pathMatch:'full'},
   {path:'login', component: LoginComponent},
   {path:'signup', component: SignupComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'region', component: RegionComponent}
 ];
 
 @NgModule({
